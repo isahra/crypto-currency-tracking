dfa1ad16b8fdf8fcc14e06cd370585c084d92381
isahra <zahrarezazade265@gmail.com>
2026-02-20 22:28:57 +0330

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import FooterComponent from "./components/ui/Footer.component";
+import NavbarComponent from "./components/ui/Navbar.component";
+
  function App() {
   return (
     <div className="App">
-       {/* navbar */}
+      <NavbarComponent/>
        {/* wrapper */}
-       {/* footer */}
+       <FooterComponent/>
     </div>
   );
 }
