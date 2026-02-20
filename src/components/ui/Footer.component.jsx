1e0711ba570f3392bf5a2cb3a0a02308631b0e8d
isahra <zahrarezazade265@gmail.com>
2026-02-20 22:27:33 +0330

    add files

---
+++ src/components/ui/Footer.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const FooterComponent = () => {
+    return ( 
+        <>
+        <h2>footer</h2>
+        </>
+     );
+}
+ 
+export default FooterComponent;
---
+++ src/components/ui/Navbar.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const NavbarComponent = () => {
+    return ( 
+        <>
+        <h2>navbar</h2>
+        </>
+     );
+}
+ 
+export default NavbarComponent;
